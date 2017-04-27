4d9bb55d96de4441d89f2b6f7f8c95d2b415c90a
Kai Kuehn <kuehn@diqa-pm.com>
2017-04-27 11:02:59 +0200

    changed extension name

--- DIQAutil.php
+++ DIQAutil.php
@@ -23,7 +23,7 @@ global $wgExtensionFunctions;
 // register extension
 $wgExtensionCredits[ 'diqa' ][] = array(
 	'path' => __FILE__,
-	'name' => 'DIQA Util',
+	'name' => 'Util',
 	'author' => array( 'DIQA Projektmanagement GmbH' ),
 	'license-name' => 'GPL-2.0+',
 	'url' => 'http://www.diqa-pm.com',

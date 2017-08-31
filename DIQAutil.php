66d7a18a3fcdbdf580dfe2a918ced3e9b6045474
Kai Kuehn <kuehn@diqa-pm.com>
2017-08-31 11:41:59 +0200

    merged from WiMa

--- DIQAutil.php
+++ DIQAutil.php
@@ -19,6 +19,7 @@ global $wgExtensionMessagesFiles;
 global $wgHooks;
 global $wgResourceModules;
 global $wgExtensionFunctions;
+global $wgAPIModules;
 
 // register extension
 $wgExtensionCredits[ 'diqa' ][] = array(
@@ -42,7 +43,7 @@ $wgHooks['ParserFirstCallInit'][] = 'wfDIQAUtilRegisterModules';
 
 //Change the image links
 $wgHooks['LinkBegin'][] = 'DIQA\Util\FileLinkUtils::ImageOverlayLinkBegin';
-
+$wgAPIModules['diqa_util_userdataapi'] = 'DIQA\Util\Api\UserDataAPI';
 
 $wgExtensionFunctions[] = function() {
 	
@@ -64,3 +65,56 @@ function wfDIQAUtilRegisterModules() {
 	
 	$wgOut->addModules('ext.diqa.util');
 }
+
+/**
+ * Makes a call to an endpoint providing wiki session cookies and PHPSESSID.
+ * 
+ * @param $proxyUrl which is responsible for ending the session. (usually a proxy)
+ * @return
+ */
+function wfDIQAUtilLogout($proxyUrl) {
+	global $wgServerHTTP, $wgScriptPath, $wgDBname;
+
+	$http = function ($url, $cookies) {
+		$res = "";
+		$header = "";
+
+		// Create a curl handle to a non-existing location
+		$ch = curl_init($url);
+
+		$cookieArray = [];
+		foreach($cookies as $key => $value) {
+			$cookieArray[] = "$key=$value";
+		}
+
+
+		// Execute
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_COOKIE, implode('; ', $cookieArray));
+		$res = curl_exec($ch);
+
+		$status = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$bodyBegin = strpos($res, "\r\n\r\n");
+		list($header, $res) = $bodyBegin !== false ? array(substr($res, 0, $bodyBegin), substr($res, $bodyBegin+4)) : array($res, "");
+		return array($header, $status, str_replace("%0A%0D%0A%0D", "\r\n\r\n", $res));
+	};
+
+	$userid = isset($_COOKIE[$wgDBname.'UserID']) ? $_COOKIE[$wgDBname.'UserID'] : '';
+	$userName = isset($_COOKIE[$wgDBname.'UserName']) ? $_COOKIE[$wgDBname.'UserName'] : '';
+	$sessionId = isset($_COOKIE[$wgDBname.'_session']) ? $_COOKIE[$wgDBname.'_session'] : '';
+
+	$PHPSESSID = isset($_COOKIE['PHPSESSID']) ? $_COOKIE['PHPSESSID'] : '';
+
+	$cookies = [
+	$wgDBname.'UserID' => $userid,
+	$wgDBname.'UserName' => $userName,
+	$wgDBname.'_session' => $sessionId,
+	'PHPSESSID' => $PHPSESSID,
+	];
+
+	$res = $http($wgServerHTTP . $wgScriptPath . $proxyUrl, $cookies);
+};
